156843f3b832845e587fa7f7738e8520d6056182
Yuliia Stanenko <yuliiastanenko@gmail.com>
2026-06-18 20:49:38 +0200

    Add arrows

--- script.js
+++ script.js
@@ -6,20 +6,45 @@ const inputHeight = document.getElementById("height");
 const section = document.querySelector("section");
 
 function createSquare() {
-  const square = document.createElement("div");
-  const sizeWidth = inputWidth.value + "px";
-  const sizeHeight = inputHeight.value + "px";
-  square.style.width = sizeWidth;
-  square.style.height = sizeHeight;
+    const square = document.createElement("div");
+    const sizeWidth = inputWidth.value + "px";
+    const sizeHeight = inputHeight.value + "px";
+    square.style.width = sizeWidth;
+    square.style.height = sizeHeight;
 
-  square.style.background = inputColor.value;
-  return square;
+    const cross = document.createElement("button");
+    cross.innerText = "❌";
+    cross.classList.add("close");
+
+    const left = document.createElement("button");
+    left.innerText = "←";
+    left.classList.add("left");
+
+    const right = document.createElement("button");
+    right.innerText = "→";
+    right.classList.add("right");
+
+    const up = document.createElement("button");
+    up.innerText = "↑";
+    up.classList.add("up");
+
+    const down = document.createElement("button");
+    down.innerText = "↓";
+    down.classList.add("down");
+
+    square.append(down, left, right, up, cross);
+
+    square.style.background = inputColor.value;
+    cross.onclick = function () {
+        square.remove();
+    }
+    return square;
 }
 
 inputNumber.onchange = function () {
-  const amount = Number(inputNumber.value);
-  for (let i = 0; i < amount; i++) {
-    const item = createSquare();
-    section.append(item);
-  }
+    const amount = Number(inputNumber.value);
+    for (let i = 0; i < amount; i++) {
+        const item = createSquare();
+        section.append(item);
+    }
 };
